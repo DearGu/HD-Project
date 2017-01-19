202870f783821902372239c9ef9f216de2826b03
DearGu <137471594@qq.com>
2017-01-19 13:17:03 +0800

    添加项目所有东西

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+﻿var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+
+gulp.task('buildSass', function() {
+	gulp.src('src/sass/index.scss')
+	    .pipe(sass())
+	    .pipe(gulp.dest('src/css'))
+});
+
+gulp.task('jtSass',function(){
+	gulp.watch('src/sass/*',['buildSass']);
+})
