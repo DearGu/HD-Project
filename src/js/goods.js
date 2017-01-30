b584cd47ab090c318c8f3103d3ff8e49c6952428
DearGu <137471594@qq.com>
2017-01-30 17:10:26 +0800

    完成商品页部分js功能

--- src/js/goods.js
+++ src/js/goods.js
@@ -5,11 +5,68 @@ $(function(){
 	var $webLinkShow = $(".web_link_show");
 	var $webLink = $webLinkShow.find("ul");
 	var floatBox = $(".float_box");
-	var $yzm = $(".yzm");
-	var resRandom = "";
-	var $registerWrap = $(".register_wrap");
-	var $registerBtn = $(".register_Btn");
-	var $quickBtn = $(".quick_Btn");
+	var $headerFr = $(".header_fr");
+	var $headerUl = $headerFr.find(">ul").eq(0);
+	var $headerDev = $(".header_dev");
+	var $sizeList = $(".size_list");
+	var $sizeVal = $(".size_val");
+	var $colorList = $(".color_list");
+	var $colorVal = $(".color_val");
+	var $minus = $(".minus");
+	var $plus = $(".plus");
+	var $buyNum = $(".buy_number");
+	var $smallShow = $(".small_show");
+	var $showZone = $(".show_zone");
+	var $bigShowZone = $(".bigshow_zone");
+	var $enlargeArea = $(".enlarge_area");
+
+	// 判断是否为登录状态
+	var cookies = document.cookie.split("; ");
+	for(var i=0;i<cookies.length;i++){
+		var arr = cookies[i].split("=");
+		if(arr[0] == "username"){
+			$headerUl.find(">li").eq(0).hide();
+			$headerUl.find(">li").eq(1).hide();
+			$headerUl.find(">li").eq(2).hide();
+
+			var $L_ul = $("<ul/>");
+			$L_ul.addClass('login_bar');
+			for(var i=0;i<4;i++){
+				var $L_li = $("<li/>");
+				$L_ul.append($L_li);
+			}
+			$headerDev.append($L_ul);
+
+			$L_ul.find('>li').eq(0).html("<a href='#' title="+arr[1]+">欢迎您,"+arr[1]+"</a><a href='#' title='注册会员'></a>");
+			$L_ul.find('>li').eq(1).html("<a href='#'>积分<strong>0</strong></a>");
+			$L_ul.find('>li').eq(2).html("<a href='#' class='out'>退出</a>");
+			var $M_div = $("<div/>");
+			$M_div.addClass('MyHD');
+			$M_div.html("<a href='#'><span>我的韩都</span></a><em></em>");
+			var $M_div_List = $("<div/>");
+			$M_div_List.addClass('MyHD_List');
+			for(var i=0;i<4;i++){
+				$("<li/>").appendTo($M_div_List);
+			}
+			$M_div.append($M_div_List);
+			$L_ul.find('>li').eq(3).append($M_div);
+			$M_div_List.find(">li").eq(0).html("<a href='#'>我的订单</a>");
+			$M_div_List.find(">li").eq(1).html("<a href='#'>我的优惠券</a>");
+			$M_div_List.find(">li").eq(2).html("<a href='#'>我的收藏</a>");
+			$M_div_List.find(">li").eq(3).html("<a href='#'>退货办理</a>");
+		}
+	}
+	// 退出按钮绑定点击事件
+		var $outBtn = $(".out");
+		$outBtn.on("click",function(){
+			var now = new Date();
+			now.setDate(now.getDate()-1);
+			document.cookie = "username=gg" + ";expires=" + now + ";path=/";
+			document.cookie = "psw=gg" + ";expires=" + now + ";path=/";
+			location.href = "http://localhost/HD/html/login.html";
+		});
+
+
 
 
 	// 悬浮盒子和悬浮搜索栏的出现和隐藏
@@ -133,8 +190,74 @@ $(function(){
 		},250);
 	});
 
-	
-	
+	// 商品信息的尺码和颜色按钮绑定点击事件
+	$sizeList.on("click","li",function(){
+		$sizeList.find("li").removeClass('attr_selected');
+		$(this).addClass('attr_selected');
+		$sizeVal.text($(this).text());
+	});
+	$colorList.on("click","li",function(){
+		$colorList.find('li').removeClass('attr_selected');
+		$(this).addClass('attr_selected');
+		$colorVal.text($(this).text());
+	});
+
+	// 购买数量的加减按钮绑定点击事件
+	$minus.on("click",function(){
+		var currentNum = parseInt($buyNum.val());
+		currentNum--;
+		if(currentNum<0){
+			currentNum = 0;
+		}
+		$buyNum.val(currentNum);
+	});
+	$plus.on("click",function(){
+		var currentNum = parseInt($buyNum.val());
+		currentNum++;
+		$buyNum.val(currentNum);
+	});
+
+	// 给商品小图片绑定点击事件
+	$smallShow.on("click","li",function(){
+		$smallShow.find('li').removeClass('sm_selected');
+		$showZone.find('img').attr("src",$(this).find('img').attr("src"));
+		$bigShowZone.find('img').attr("src",$(this).find('img').attr("src"));
+		$(this).addClass('sm_selected');
+	});
+
+	// 放大镜功能
+	$showZone.on("mouseenter",function(){
+		$enlargeArea.show();
+		$bigShowZone.fadeIn();
+		$(document).on("mousemove",function(e){
+			$enlargeArea.css({
+				left:e.clientX - $showZone.offset().left - $enlargeArea.outerWidth()/2,
+				top:e.clientY - $showZone.offset().top - $enlargeArea.outerHeight()/2 + $(window).scrollTop()
+			});
+			if(parseInt($enlargeArea.offset().left) <= $showZone.offset().left){
+				$enlargeArea.css("left",0);
+			}
+			if(parseInt($enlargeArea.offset().left) >= $showZone.offset().left + $showZone.outerWidth()- $enlargeArea.outerWidth()){
+				$enlargeArea.css("left",$showZone.outerWidth()- $enlargeArea.outerWidth());
+			}
+			if(parseInt($enlargeArea.offset().top) <= $showZone.offset().top){
+				$enlargeArea.css("top",0);
+			}
+			if(parseInt($enlargeArea.offset().top) >= $showZone.offset().top + $showZone.outerHeight()- $enlargeArea.outerHeight()){
+				$enlargeArea.css("top",$showZone.outerHeight()- $enlargeArea.outerHeight());
+			}
+			$bigShowZone.find("img").css({
+				left:parseInt($enlargeArea.css("left"))*-(800/480),
+				top:parseInt($enlargeArea.css("top"))*-(800/480)
+			})
+		});	
+		
+	});
+	$showZone.on("mouseleave",function(){
+		$enlargeArea.hide();
+		$bigShowZone.fadeOut();
+		$(document).off("mousemove");
+	});
 
 
 
